2965d6d8c89348e7a52bf730995179c95e463e7b
Jon Cuthbert <jon@cuth.net>
2015-11-17 16:04:19 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+js-module-boilerplate
+=====================
+
+This is a boilerplate for C2 JavaScript modules to speed up development. It uses Rollup to transpile ES6 to CommonJS, browser globals (IIFE), and UMD.
---
+++ rollup-umd.config.js
@@ -0,0 +1,26 @@
+import babel from 'rollup-plugin-babel';
+
+const pjson = require('./package.json');
+
+const banner = `/*!
+ * ${pjson.name}
+ * ${pjson.repository.url}
+ * @version ${pjson.version}
+ * @license ${pjson.license}
+ */`;
+
+const filename = pjson['jsnext:main'];
+
+export default {
+    entry: filename,
+    dest: `umd/${filename}`,
+    format: 'umd',
+    plugins: [
+        babel()
+    ],
+    moduleName: pjson['export'],
+    banner,
+    globals: {
+        jquery: 'jQuery'
+    }
+};
